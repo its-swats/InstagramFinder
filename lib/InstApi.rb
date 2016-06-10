257067ac8af8c2a73b5ba9598db11fb5a05fd0b7
Shawn Watson <shawnw86@gmail.com>
2016-06-10 00:30:55 -0700

    Extract tag_time from comments if tag was added in comments by the image poster

--- lib/InstApi.rb
+++ lib/InstApi.rb
@@ -4,41 +4,40 @@ module InstApi
   PATH = "/v1/"
 
   module POSTS
-    
-    def self.posts(hashtag, secret_key, start_date, end_date, pagination = '')
-      url_str = url(hashtag, secret_key, start_date, end_date, pagination)
-      posts = get(url)
-      return JSON.parse(posts).body
+    def self.posts(hashtag, start_date, end_date, pagination = '')
+      url_str = url(hashtag, start_date, end_date, pagination)
+      posts = get(url_str)
+      return JSON.parse(posts.body)
     end
 
     private
 
-    def self.url(hashtag, secret_key, start_date, end_date, pagination = '')
-      return PROTOCOL + HOST + PATH + "tags/#{hashtag}/media/recent?access_token=#{secret_key}&min_timestamp=#{start_date}&max_timestamp=#{end_date}#{pagination}"
+    def self.url(hashtag, start_date, end_date, pagination)
+      return PROTOCOL + HOST + PATH + "tags/#{hashtag}/media/recent?access_token=#{SECRET_KEY}&min_timestamp=#{start_date}&max_timestamp=#{end_date}#{pagination}"
     end
 
     def self.get(url)
-      return HTTParty(url)
+      return HTTParty.get(url)
     end
 
   end
 
   module COMMENTS
-
-    def self.comments
-      url_str = url(id, secret_key)
-      comments = get(url)
-      return JSON.parse(comments).body
+    include HTTParty
+    def self.comments(id)
+      url_str = url(id)
+      comments = get(url_str)
+      return JSON.parse(comments.body)
     end
 
     private
 
-    def self.url(id, secret_key)
-      return PROTOCOL + HOST + PATH + "media/#{id}/comments?access_token=#{secret_key}"
+    def self.url(id)
+      return PROTOCOL + HOST + PATH + "media/#{id}/comments?access_token=#{SECRET_KEY}"
     end
 
-    def sefget(url)
-      return HTTParty(url)
+    def self.get(url)
+      return HTTParty.get(url)
     end
 
   end
