72827cd33b7a72a889ffee504158862cfa83e33c
Shawn Watson <shawnw86@gmail.com>
2016-06-09 18:04:36 -0700

    Update Instagram class to handle pagination

--- app/models/instagram.rb
+++ app/models/instagram.rb
@@ -4,6 +4,7 @@ class Instagram
 	def initialize(hashtag)
 		@hashtag = hashtag
 		@parsed_data = get_data_from_api
+
 	end
 
 	def create_instagram_collection
@@ -13,8 +14,8 @@ class Instagram
 
 	private
 
-	def get_data_from_api
-		JSON.parse(HTTParty.get("https://api.instagram.com/v1/tags/#{@hashtag}/media/recent?access_token=#{SECRET_KEY}").body)['data']
+	def get_data_from_api(pagination = '')
+		JSON.parse(HTTParty.get("https://api.instagram.com/v1/tags/#{@hashtag}/media/recent?access_token=#{SECRET_KEY}#{pagination}").body)
 	end
 
 	def create_collection
@@ -24,11 +25,18 @@ class Instagram
 	end
 
 	def create_posts
-		@parsed_data.each do |post|
+		@parsed_data['data'].each do |post|
 			new_post = Post.find_by(instagram_id: post['id']) || Post.new(file_type: post['type'], caption: post['caption'], username: post['user']['username'], instagram_id: post['id'], video: post['videos'], image: post['images'])		
 			new_post.collections << @collection
 			new_post.tags << @collection.tag
 			new_post.save if !new_post.persisted?
 		end
+
+		handle_pagination if @parsed_data['pagination']['next_url']
+	end
+
+	def handle_pagination
+			@parsed_data = get_data_from_api("&max_tag_id=#{@parsed_data['pagination']['next_max_tag_id']}")
+			create_posts
 	end
 end
