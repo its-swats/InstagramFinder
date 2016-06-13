1b3a3a17c2d4d516a0870eeec27325d390548d1d
Shawn Watson <shawnw86@gmail.com>
2016-06-12 21:34:36 -0700

    Add some extra error handling for form params, and reformat hashtags entered with invalid characters

--- app/controllers/collection_controller.rb
+++ app/controllers/collection_controller.rb
@@ -1,24 +1,36 @@
+require 'ErrorHandling'
+
 class CollectionController < ApplicationController
 	protect_from_forgery
 
 	def index
-		@hashtag = Tag.find_or_create_by(hashtag: params[:request][:hashtag]).downcase
-		start_date = Date.parse(params[:request][:start_date]).to_time.to_i
-		end_date = (Date.parse(params[:request][:end_date]).to_time.to_i + 86400)		
-
-		@all_posts = @hashtag.posts.where("tag_time < ?", end_date).where("tag_time > ?", start_date)
-		@cover = @all_posts[0]
-		@pages = @all_posts[1..-1]
-		if @cover == nil
-			flash[:errors] = "No entries for #{@hashtag.hashtag} found - please scrape first."
+		if ErrorHandling.verify_form_params(params[:request])
+			@hashtag = Tag.find_or_create_by(hashtag: params[:request][:hashtag].downcase.gsub(/[^0-9a-z]/i, '')) 
+			@all_posts = @hashtag.posts.where("tag_time < ?", (Date.parse(params[:request][:end_date]).to_time.to_i + 86400))
+																 .where("tag_time > ?", Date.parse(params[:request][:start_date]).to_time.to_i)
+			@cover = @all_posts[0]
+			@pages = @all_posts[1..-1]
+			if @cover == nil
+				flash[:errors] = "No entries for #{@hashtag.hashtag} found - please scrape first."
+				redirect_to root_path
+			end
+		else
+			flash[:errors] = "Your search terms were not valid, please try again."
 			redirect_to root_path
 		end
 	end
 
 	def create
-		collection = Instagram.new(params[:request][:hashtag], params[:request][:start_date], params[:request][:end_date])
-		collection.create_instagram_collection
-		redirect_to root_path
+		if ErrorHandling.verify_form_params(params[:request])
+			collection = Instagram.new(params[:request][:hashtag].gsub(/[^0-9a-z]/i, ''), 
+																 params[:request][:start_date], params[:request][:end_date])
+			collection.create_instagram_collection
+			redirect_to root_path
+		else
+			flash[:errors] = "Your search terms were not valid, please try again."
+			redirect_to root_path
+		end
 	end
-
 end
+
+
