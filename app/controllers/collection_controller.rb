a89562a02c0123a035fa2d6847a77e5d68ac8b2c
Shawn Watson <shawnw86@gmail.com>
2016-06-12 20:41:14 -0700

    Update CSS and Javascript, disable form submission until data is acceptable

--- app/controllers/collection_controller.rb
+++ app/controllers/collection_controller.rb
@@ -2,10 +2,9 @@ class CollectionController < ApplicationController
 	protect_from_forgery
 
 	def index
-		@hashtag = Tag.find_or_create_by(hashtag: params[:request][:hashtag])
+		@hashtag = Tag.find_or_create_by(hashtag: params[:request][:hashtag]).downcase
 		start_date = Date.parse(params[:request][:start_date]).to_time.to_i
 		end_date = (Date.parse(params[:request][:end_date]).to_time.to_i + 86400)		
-
 
 		@all_posts = @hashtag.posts.where("tag_time < ?", end_date).where("tag_time > ?", start_date)
 		@cover = @all_posts[0]
